3fb0a592a88a864c62f9f67a80ce84d92f7fc4d3
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-15 16:05:42 -0300

    feat: req 2 - cria sales controller

---
+++ backend/src/controllers/sales.controller.js
@@ -0,0 +1,19 @@
+const { salesService } = require('../services');
+
+const statusHTTP = require('../utils/statusHttp');
+
+async function findAllSales(_req, res) {
+  const allSales = await salesService.getAllSales(); // criar getAllProducts na camada Services
+  return res.status(statusHTTP(allSales.status)).json(allSales.data);
+}
+
+async function findSalesById(req, res) {
+  const { id } = req.params;
+  const sales = await salesService.getSalesById(id); // criar getSalesById na camada Sevices
+  return res.status(statusHTTP(sales.status)).json(sales.data);
+}
+
+module.exports = {
+  findAllSales,
+  findSalesById,
+};
